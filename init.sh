0a71899859a82d9d1f18a5b6bf1819bbe2d6576f
宿仕华 <>
2020-11-10 19:48:06 +0800

    uunzip不显示解压过程

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ mkdir paddle_tongdao
 cd paddle_tongdao
 mkdir anns
 mkdir imgs
-unzip -P $UNZIP_PSW -d ~/data/paddle_tongdao/anns/ ~/data/data59386/anns.zip
-unzip -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs01.zip
-unzip -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs02.zip
-unzip -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs03.zip
-unzip -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs04.zip
+unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/anns/ ~/data/data59386/anns.zip
+unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs01.zip
+unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs02.zip
+unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs03.zip
+unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs04.zip
