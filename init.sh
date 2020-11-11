9b322a1b1e9bd643219467b6eb8b329f97a674c0
宿仕华 <>
2020-11-11 22:04:29 +0800

    补充init.sh，添加xiaozhi

--- init.sh
+++ init.sh
@@ -10,3 +10,10 @@ unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs01.zi
 unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs02.zip
 unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs03.zip
 unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59386/imgs04.zip
+
+#xiaozhi
+#unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/anns/ ~/data/data59684/anns.zip
+#unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59684/imgs01.zip
+#unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59684/imgs02.zip
+#unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59684/imgs03.zip
+#unzip -q -P $UNZIP_PSW -d ~/data/paddle_tongdao/imgs/ ~/data/data59684/imgs04.zip
